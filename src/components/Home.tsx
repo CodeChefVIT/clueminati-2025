51d2fbc0e9c8d56677ec88837f609c32ceb462fc
Rohith-JN <rohithnambiar04@gmail.com>
2025-09-09 18:08:28 +0530

    feat: instructions folder

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,26 +1,11 @@
+
 "use client";
 
 export default function Home() {
   return (
-    <div className="w-full flex flex-col items-center justify-start text-white min-h-screen p-4">
-      <div className="relative w-[100%] max-w-md bg-center bg-contain bg-no-repeat flex flex-col items-center justify-center text-white aspect-[3/4]"
-        style={{ backgroundImage: "url('/assets/instructions_bg.svg')" }}
-      >
-        <h2 className="text-lg sm:text-xl font-bold mb-4 mt-12 text-[#A5A5A5]">Instructions</h2>
+    <div className="w-full flex flex-col items-center justify-start text-white min-h-[calc(100vh-7rem)] p-8 sm:p-12">
+      <h1 className="text-3xl sm:text-4xl font-bold mb-6">Home</h1>
 
-        <ul className="space-y-2 text-center text-[#A5A5A5] text-sm sm:text-base">
-          <li>Instructions</li>
-          <li>Instructions</li>
-          <li>Instructions</li>
-          <li>Instructions</li>
-          <li>Instructions</li>
-        </ul>
-      </div>
-
-      <div
-        className="mt-6 w-[60%] max-w-xs h-23 bg-center bg-contain bg-no-repeat flex items-center justify-center text-white font-bold tracking-wide cursor-pointer"
-        style={{ backgroundImage: "url('/assets/proceed.svg')" }}
-      />
     </div>
   );
 }
