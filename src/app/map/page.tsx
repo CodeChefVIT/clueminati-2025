00e2e09f2b78b8c3b765cbf8b22a9551ceda570f
unknown <sanjana.shyamsundar55@gmail.com>
2025-09-20 03:10:13 +0530

    Fix : Layout and remove scroll

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -6,23 +6,28 @@ const Brick="/assets/brick.png";
 
 export default function MapScreen() {
   return (
-  <div className="w-[600px] h-[600px] flex items-center justify-center p-4 bg-transparent ">
-      <div className="relative w-full max-w-[200px] bg-white rounded-lg p-1.5 shadow-2xl -mt-40 left-0">
+    
+  <div className="screen relative overflow-hidden  inset-0 flex items-center justify-center p-4 bg-transparent ">
+      <div className="absolute top-7 left-20 w-[200px] h-auto bg-white rounded-lg p-1.5 shadow-2xl"
+       style={{
+          touchAction: 'none',
+          overscrollBehavior: 'none'
+        }}>
         <img
           src={mapSvgPath}
           alt="Game Map"
           className="w-full h-auto rounded-md" 
         />
       </div>
-      <div className="relative w-full max-w-[263px] rounded-lg p-1.5 shadow-2xl right-59 mt-77">
+      <div className="relative overflow-visible flex items-center justify-center top-[170px] w-full rounded-lg p-20 shadow-2xl absolute right-[32px] mt-[290px]  ">
         <img
           src={Brick}
           alt="Game Map"
-          className="w-full h-auto rounded-md" 
+          className="absolute bottom-20 right-10 w-[220px] h-[220px] object-contain " 
         />
       </div>
-    <div className="max-w-[40px] rounded-lg  right-66 mt-190">
-        <span className="absolute inset-0 bg-transparent text-center text-xs sm:text-sm text-white font-semibold focus:outline-none px-10 py-110">
+    <div className=" max-w-[40px] rounded-lg ">
+<span className="absolute inset-0 -left-[20px] bg-transparent text-center text-[0.52rem] sm:text-sm text-white font-semibold focus:outline-none px-20 py-110">
                 The areas marked in green are your stations
               </span>
       </div>
