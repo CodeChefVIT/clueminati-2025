962ed1f7a93947c46e193eca4bd69b58d8082a57
Ayman <299.bluelock@gmail.com>
2025-09-23 00:57:13 +0530

    Update team.ts

--- src/lib/models/team.ts
+++ src/lib/models/team.ts
@@ -23,9 +23,7 @@ const TeamSchema = new Schema<ITeam>({
       hard: { type: [{ type: String, ref: "Questions" }], default: [] },
     },
     score: { type: Number, default: 0 },
-    indoor_score: { type: Number, default: 0 },
-    // testing skip - add skip tracking
-    lastSkipTimestamp: { type: Date, required: false },
+    indoor_score: { type: Number, default: 0 }
   },
   round2: {
     questions_encountered: {
@@ -46,11 +44,10 @@ const TeamSchema = new Schema<ITeam>({
     solvedStations: { type: [String], default: [] },
     secret_string: { type: String, required: false },
     secret_chars_revealed: { type: Number, default: 0 },
-    letters_found: { type: [String], default: [] },
-    // testing skip - add skip tracking
-    lastSkipTimestamp: { type: Date, required: false },
+    letters_found: { type: [String], default: [] }
   },
   total_score: { type: Number, default: 0, required: true },
+  //teamString: { type: String, required: false },
 });
 
 const Team: Model<ITeam> = models.Team ?? model<ITeam>("Team", TeamSchema);
