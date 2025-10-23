9ed6e2544014f916e758a2bebe2ae04d08493f3c
Dhruv Sabharwal <dhruvsabharwalh7@gmail.com>
2025-10-24 02:10:42 +0530

    feat: add copy team code button

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,22 +1,26 @@
 import type { ReactNode } from "react";
 import "./globals.css";
 import LayoutClientWrapper from "@/components/LayoutWrapper";
+import { Toaster } from "react-hot-toast";
 
 export const metadata = {
-  title: "Clueminati 2025",
-  icons: {
-    icon: "/icon-192x192.png"
-  },
-  
-}
-
+	title: "Clueminati 2025",
+	icons: {
+		icon: "/icon-192x192.png",
+	},
+};
 
 export default function RootLayout({ children }: { children: ReactNode }) {
-  return (
-    <html lang="en">
-      <body>
-        <LayoutClientWrapper>{children}</LayoutClientWrapper> {/* this for keeping layout.tsx ssr  */}
-      </body>
-    </html>
-  );
+	return (
+		<html lang="en">
+			<body>
+				<Toaster
+					position="top-center"
+					reverseOrder={false}
+				/>
+				<LayoutClientWrapper>{children}</LayoutClientWrapper>{" "}
+				{/* this for keeping layout.tsx ssr  */}
+			</body>
+		</html>
+	);
 }
